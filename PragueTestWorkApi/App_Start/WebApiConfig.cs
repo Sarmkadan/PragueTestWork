3cabfb88b7a5b0e185f704fd5873ad512388226a
Vlad Zaech <rutova2@gmail.com>
2019-07-09 10:52:38 +0300

    Добавьте файлы проекта.

---
+++ PragueTestWorkApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace PragueTestWorkApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Конфигурация и службы веб-API
+
+            // Маршруты веб-API
+            config.MapHttpAttributeRoutes();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
